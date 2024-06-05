aae340236848249c41307799db82fc9624f689dc
Alex Silva <alexsilvaddt17@gmail.com>
2024-06-05 14:02:39 -0400

    Subiendo el primer desafío del módulo de JS de Desaío Latam

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const precio = 400000;
+let cantidad = 0;
+let totalAPagar = 0;
+
+document.querySelector("#precioBase").innerHTML = precio;
+document.querySelector("#cantidad").innerHTML = cantidad;
+document.querySelector("#totalAPagar").innerHTML = totalAPagar;
+
