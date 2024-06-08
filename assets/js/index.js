256b5fb0f0fec6edcc174b65b6fbd611264aa8c2
Alex Silva <alexsilvaddt17@gmail.com>
2024-06-08 19:59:04 -0400

    Refactorizando el código aplicando funciones y condiciones

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,23 @@ const precio = 400000;
 let cantidad = 0;
 let totalAPagar = 0;
 
-document.querySelector("#precioBase").innerHTML = precio;
-document.querySelector("#cantidad").innerHTML = cantidad;
-document.querySelector("#totalAPagar").innerHTML = totalAPagar;
+// Función para aumentar en 1 la cantidad de computadores hasta 5
+function aumentarCantidad() {
+    if( cantidad < 5 ) {  // Condición que establece el máximo de computadores
+        cantidad += 1;
+        document.querySelector('#cantidad').innerHTML = cantidad;
+        totalAPagar = cantidad * precio;
+        document.querySelector('#totalAPagar').innerHTML = totalAPagar;
+    }
+};
+
+// Función para reducir de 1 en 1 la cantidad de computadores hasta 0
+function disminuirCantidad() {
+    if( cantidad >0) {  // Con esto evitamos que la cantidad sea negativa
+        cantidad -= 1;
+        document.querySelector('#cantidad').innerHTML = cantidad;
+        totalAPagar = cantidad * precio;
+        document.querySelector('#totalAPagar').innerHTML = totalAPagar;
+    }
+}
 
