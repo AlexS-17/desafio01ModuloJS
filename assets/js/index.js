3826d75fc66545a9f8ab1b392a3b53beeaee2b0f
Alex Silva <alexsilvaddt17@gmail.com>
2024-06-15 16:01:29 -0400

    Mejorando el código

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,23 +2,27 @@ const precio = 400000;
 let cantidad = 0;
 let totalAPagar = 0;
 
+// Función para actualizar el DOM con la cantidad y el total a pagar
+function actualizarDOM() {
+    document.querySelector('#cantidad').textContent = cantidad;
+    document.querySelector('#totalAPagar').textContent = totalAPagar;
+}
+
 // Función para aumentar en 1 la cantidad de computadores hasta 5
 function aumentarCantidad() {
     if( cantidad < 5 ) {  // Condición que establece el máximo de computadores
         cantidad += 1;
-        document.querySelector('#cantidad').innerHTML = cantidad;
         totalAPagar = cantidad * precio;
-        document.querySelector('#totalAPagar').innerHTML = totalAPagar;
+        actualizarDOM()
     }
 };
 
 // Función para reducir de 1 en 1 la cantidad de computadores hasta 0
 function disminuirCantidad() {
-    if( cantidad >0) {  // Con esto evitamos que la cantidad sea negativa
+    if( cantidad > 0) {  // Con esto evitamos que la cantidad sea negativa
         cantidad -= 1;
-        document.querySelector('#cantidad').innerHTML = cantidad;
         totalAPagar = cantidad * precio;
-        document.querySelector('#totalAPagar').innerHTML = totalAPagar;
+        actualizarDOM()
     }
 }
 
